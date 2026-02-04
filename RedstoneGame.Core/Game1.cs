2aeb9a5a15984d8c258deddb9eebbcbf96daee6d
lkmqdoge <qdoge2335@gmail.com>
2026-02-04 20:31:52 +0300

    update from laptop

--- RedstoneGame.Core/Game1.cs
+++ RedstoneGame.Core/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Lkmqdoge.Monogame;
+﻿using System.Collections.Generic;
+
+using Lkmqdoge.Monogame;
 using Lkmqdoge.Monogame.Graphics;
 using Lkmqdoge.Monogame.Input;
 
@@ -6,14 +8,17 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-using RedstoneGame.Core.Entity.Projectile;
+using RedstoneGame.Core.World;
 
 namespace RedstoneGame.Core;
 
 public class Game1 : GameCore
 {
     private readonly Label2D _label = new();
+
     private SpriteFont _font;
+
+    private readonly List<GameObject> _gameObjects = [];
 
     public Game1() : base()
     {
@@ -22,6 +27,9 @@ public class Game1 : GameCore
         );
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        // главная сцена
+        AddGameObject(new MineSweeperField(0, 0, 10, 10));
     }
 
     protected override void Initialize()
@@ -35,6 +43,9 @@ public class Game1 : GameCore
         _font = Content.Load<SpriteFont>("Fonts/debug");
         _label.Font = _font;
         _label.Text = "Hello world";
+
+        foreach(var obj in _gameObjects)
+            obj.LoadContent(Content);
     }
 
     protected override void Update(GameTime gameTime)
@@ -43,14 +54,25 @@ public class Game1 : GameCore
 
         if (InputManager.IsActionJustPressed("debug_exit"))
             Exit();
+
+        foreach(var obj in _gameObjects)
+            obj.Tick(gameTime); // FIX
     }
 
     protected override void Draw(GameTime gameTime)
     {
         base.Draw(gameTime);
+
         SpriteBatch.Begin();
         _label.Draw(SpriteBatch);
+        foreach(var obj in _gameObjects)
+            obj.Draw(SpriteBatch, gameTime);
         SpriteBatch.End();
+    }
 
+    public void AddGameObject(GameObject gameObject)
+    {
+        _gameObjects.Add(gameObject);
+        gameObject.Start();
     }
 }
--- RedstoneGame.Core/World/MineSweeperField.cs
+++ RedstoneGame.Core/World/MineSweeperField.cs
@@ -1,18 +1,31 @@
+using Lkmqdoge.Monogame.Graphics;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace RedstoneGame.Core.World;
 
-public class MineSweeperField
+public class MineSweeperField : GameObject
 {
     public MineSweeperCell[,] Cells { get; set; }
 
+    public Vector2 Position { get; set; }
+
+    private readonly Sprite2D _testSprite = new (); 
+
     public MineSweeperField(int x, int y, int w, int h)
     {
-
+        _testSprite.Position = new (x, y);
+        _testSprite.Scale = 5.0f;
     }
 
-    public void Draw(SpriteBatch spriteBatch)
+    public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content) 
     {
+        _testSprite.Texture = content.Load<Texture2D>("Textures/minesweeper");
+    }
 
+    public override void  Draw(SpriteBatch spriteBatch, GameTime gameTime)
+    {
+        _testSprite.Draw(spriteBatch);
     }
 }
